9e5def9131e620ee079b2fbd984979aa9139ac92
karazKOS <ykaraiscos@yahoo.com>
2020-11-06 12:02:44 -0800

    added functions that allow additions of roles, employees, etc.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var inquirer = require("inquirer");
 var connection = require("./connection");
 var cTable = require("console.table");
 var mysql = require("mysql");
+const { connect } = require("./connection");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,14 +14,18 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
+  mainMenu();
   console.log("connected as id " + connection.threadId);
 });
 
 const viewOptions = [
-  "View Departments",
-  "View Roles",
-  "View Employees",
-  "Update Employee",
+  "Add a department",
+  "Add a role",
+  "Add an employee",
+  "View departments",
+  "View roles",
+  "View employees",
+  "Update employee",
   "Exit",
 ];
 
@@ -37,6 +42,26 @@ const employeeOptions = [
 
 const updateOptions = ["First Name", "Last Name", "Role", "exit"];
 
+function mainMenu() {
+  inquirer
+    .prompt({
+      name: "menu",
+      type: "confirm",
+      message: "Do you want to add, view or update the data?",
+    })
+    .then(function (res) {
+      switch (res.menu) {
+        case true:
+          runSearch();
+          break;
+
+        case false:
+          connection.end();
+          break;
+      }
+    });
+}
+
 runSearch();
 
 function runSearch() {
@@ -50,25 +75,147 @@ function runSearch() {
     .then(function (answer) {
       switch (answer.action) {
         case viewOptions[0]:
-          departmentView();
+          addDepartment();
           break;
 
         case viewOptions[1]:
-          roleView();
+          addRole();
           break;
 
         case viewOptions[2]:
-          employeeView();
+          addEmployee();
           break;
 
         case viewOptions[3]:
-          connection.end();
+          departmentView();
           break;
 
         case viewOptions[4]:
+          roleView();
+          break;
+
+        case viewOptions[5]:
+          employeeView();
+          break;
+
+        case viewOptions[6]:
+          connection.end();
+          break;
+
+        case viewOptions[7]:
           connection.end();
           break;
       }
+    });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt({
+      name: "newDepartment",
+      type: "input",
+      message: "What is the department name?",
+    })
+    .then(function (res) {
+      var query = connection.query(
+        "INSERT INTO department SET ?",
+        {
+          name: res.newDepartment,
+        },
+        function (err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + "department added");
+          mainMenu();
+        }
+      );
+      console.log(query.sql);
+    });
+}
+
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        name: "newRole",
+        type: "input",
+        message: "What is the name of this role?",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "How much is the salary?",
+
+        validate: function (value) {
+          if (isNaN(parseFloat(value))) {
+            console.log("\n Please enter a decimal value.");
+            return false;
+          }
+          return true;
+        },
+      },
+      {
+        name: "departmentID",
+        type: "input",
+        message: "What is the dept. ID number?",
+      },
+    ])
+    .then(function (res) {
+      console.log(res);
+      var query = connection.query(
+        "INSERT INTO role SET ?",
+        {
+          title: res.newRole,
+          salary: parseFloat(res.salary).toFixed(2),
+          department_id: res.department_id,
+        },
+        function (err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + "Role Added");
+          mainMenu();
+        }
+      );
+      console.log(query.sql);
+    });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "firstName",
+        type: "input",
+        message: "What is the employee's first name?",
+      },
+      {
+        name: "lastName",
+        type: "input",
+        message: "What is the employee's last name?",
+      },
+      {
+        name: "roleID",
+        type: "input",
+        message: "What is this employee's role number?",
+      },
+      {
+        name: "managerID",
+        type: "input",
+        message: "What is this employee's manager's ID number?",
+      },
+    ])
+
+    .then(function (res) {
+      var query = connection.query(
+        "INSERT INTO department SET ?",
+        {
+          name: res.newDepartment,
+        },
+        function (err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + "department added");
+          mainMenu();
+        }
+      );
+      console.log(query.sql);
     });
 }
 
